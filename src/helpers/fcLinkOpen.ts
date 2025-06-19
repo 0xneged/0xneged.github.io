cec7944c8bb54dabf8114e01e120c077e193b798
T_DaMER <dpancov64@gmail.com>
2025-06-19 14:35:15 +0300

    update sharing

--- src/helpers/fcLinkOpen.ts
+++ src/helpers/fcLinkOpen.ts
@@ -1,4 +1,5 @@
 import { sdk } from '@farcaster/frame-sdk'
+import env from './env'
 
 const { viewProfile, openUrl, composeCast } = sdk.actions
 
@@ -16,6 +17,6 @@ export default function fcLinkOpen({
 export function sharePost(address: string) {
   void composeCast({
     text: 'get Rich or get Rekt',
-    embeds: [`https://0xneged.github.io?ref=${address}`],
+    embeds: [`${env.VITE_APP_URL}?ref=${address}`],
   })
 }
